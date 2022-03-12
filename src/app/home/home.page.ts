2ef9f176f2da92b4fe340c0b970e38a544e9c0d1
LuisMa2609 <luisma--munguia1@live.com.mx>
2022-03-11 18:40:20 -0600

    BT incompleto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
 
+import { BluetoothSerial } from '@awesome-cordova-plugins/bluetooth-serial/ngx';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+Devices
+  constructor(private bluetootSerial:BluetoothSerial,private alerController:AlertController) {}
 
-  constructor() {}
+  BTon(){
+    this.bluetootSerial.isEnabled().then(Response=>{
+      this.isEnabled("Bluetooth encendido");
+      this.Listevices()
+    },error=>{
+      this.isEnabled("Bluetooth apagado")
+    })
+  }
+
+  Listevices(){
+    this.bluetootSerial.list(),then(response=>{
+      this.Devices :response
+    },error =>{
+      console.log("error")
+    })
+  }
+
+  async isEnabled(msg){
+    const alert = await this.alerController.create({
+      header:'Alerta',
+      message: msg,
+      buttons:[{
+        text:'okay',
+        handler:()=>{
+          console.log("okay")
+        }
+
+      }]
+    })
+  }
 
 }
