5d934a7603ea4a9035d240753d329d8510e533be
a20200439LuisMunguia <91489270+a20200439LuisMunguia@users.noreply.github.com>
2022-03-16 20:10:02 -0600

    Update home.module.ts

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,13 +7,18 @@ import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 import { BluetoothSerial } from '@awesome-cordova-plugins/bluetooth-serial/ngx';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [HomePage],
   providers:[
